e8d0a9579212ab7553b9a67bc35205a3043cf05a
kmj <kemengjian@126.com>
2024-05-24 00:35:37 +0800

    hyprland catppuccin mocha

--- software/gui/hyprland/default.nix
+++ software/gui/hyprland/default.nix
@@ -5,6 +5,13 @@
   wayland.windowManager.hyprland = {
     enable = true;
     extraConfig = builtins.readFile ./hyprland.conf;
+    settings = {
+      source = [
+        "~/.config/hypr/mocha.conf"
+      ];
+    };
   };
+
+  home.file.".config/hypr/mocha.conf".source = ./mocha.conf;
 
 }
