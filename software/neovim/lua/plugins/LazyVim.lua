0b6f6bd0e738b3e1967175fb81e5ccabff839ddb
kemengjian <kemengjian@126.com>
2026-08-18 22:21:46 +0800

    markdown redner

--- software/neovim/lua/plugins/LazyVim.lua
+++ software/neovim/lua/plugins/LazyVim.lua
@@ -5,5 +5,14 @@ return {
     opts = {
       colorscheme = "catppuccin",
     },
+    init = function()
+      vim.opt.spell = false
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "markdown", "text", "gitcommit" },
+        callback = function()
+          vim.opt_local.spell = false
+        end,
+      })
+    end,
   },
 }
---
+++ software/neovim/lua/plugins/render-markdown.lua
@@ -0,0 +1,12 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+  config = function(_, opts)
+    require("render-markdown").setup(opts)
+    Snacks.toggle({
+      name = "Render Markdown",
+      get = require("render-markdown").get,
+      set = require("render-markdown").set,
+    }):map("<leader>um")
+  end,
+}
